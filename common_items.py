ae468064fbc9665d58709d03640d7628aae045db
BeidouChangxi <beidoustar@gmail.com>
2025-10-28 11:01:59 +0800

    Add suppport for factor, offset and unit

--- common_items.py
+++ common_items.py
@@ -1,5 +1,5 @@
 from construct import (
-    Lazy, LazyBound, Peek, 
+    Lazy, LazyBound, Peek, Adapter,
     Error, Probe, this,
     Switch, RepeatUntil, GreedyRange,
     Bytes, GreedyBytes, 
@@ -13,3 +13,66 @@ GB/T 32960.3-2016 chp6.4 table5
 GB/T 32960.3-2025 chp6.4 table5
 """
 rtm_ts = Int8ub[6]
+
+"""
+Handle Numbers with factor, offset and unit
+"""
+class DataItem(object):
+    def __init__(self, value, unit, validity):
+        self.value = value
+        self.unit = unit
+        self.valid = validity
+    def __repr__(self):
+        if self.valid:
+            return "<DataItem %s %s>" % (self.value, self.unit)
+        elif self.valid==None:
+            return "<DataItem Invalid>"
+        else:
+            return "<DataItem Abnormal>"
+    
+    def __str__(self):
+        if self.valid:
+            return "%s %s" % (self.value, self.unit)
+        elif self.valid==None:
+            return "Invalid"
+        else:
+            return "Abnormal"
+    
+    def __int__(self):
+        return int(self.value)
+    
+    def __float__(self):
+        return float(self.value)
+    
+    def __bool__(self):
+        return bool(self.value)
+    
+    def __eq__(self, other):
+        if isinstance(other, PhysicalValue):
+            return self.value == other.value and self.unit == other.unit
+        return self.value == other
+
+class DataItemAdapter(Adapter):
+    def __init__(self, subcon, unit, factor=1, offset=0, *, validation=True):
+        super().__init__(subcon)
+        self.factor = factor
+        self.offset = offset
+        self.unit = unit
+        self.validation = validation
+        self.abnormal_value = 2**(self.sizeof()*8)-2
+        self.invalid_value = 2**(self.sizeof()*8)-1
+    
+    def _decode(self, raw_value, context, path):
+        validity = True
+        if self.validation:
+            if raw_value==self.abnormal_value:
+                validity = False
+            elif raw_value==self.invalid_value:
+                validity = None
+        return DataItem(raw_value*self.factor + self.offset, self.unit, validity)
+    
+    def _encode(self, phy_value, context, path):
+        if isinstance(phy_value, DataItem):
+            phy_value = phy_value.value
+        raw_value = (phy_value-self.offset)/self.factor
+        return int(round(raw_value))
--- data_items.py
+++ data_items.py
@@ -5,34 +5,36 @@ GB/T 32960.3-2016 chp7.2.3.1 table9
 GB/T 32960.3-2016 anxB.3.5.3.1 tableB.4
 """
 whole_vehicle_data_2016 = Struct(
-    "vehicle_state" / Int8ub,
-    "charge_state" / Int8ub,
-    "operate_state" / Int8ub,
-    "vehicle_speed" / Int16ub,
-    "mileage_total" / Int32ub,
-    "voltage_total" / Int16ub,
-    "current_total" / Int16ub,
-    "soc" / Int8ub,
-    "dcdc_state" / Int8ub,
+    "vehicle_state" / Enum(Int8ub, on=0x01, off=0x03, other=0x03, abnormal=0xfe, invalid=0xff),
+    "charge_state" / Enum(Int8ub, parking_charging=0x01, driving_charging=0x02, uncharged=0x03, completed_charging=0x04, abnormal=0xfe, invalid=0xff),
+    "operate_state" / Enum(Int8ub, pure_electric=0x01, hybrid=0x02, fuel=0x03, abnormal=0xfe, invalid=0xff),
+    "vehicle_speed" / DataItemAdapter(Int16ub, "km/h", 0.1),
+    "mileage_total" / DataItemAdapter(Int32ub, "km", 0.1),
+    "voltage_total" / DataItemAdapter(Int16ub, "V", 0.1),
+    "current_total" / DataItemAdapter(Int16ub, "A", 0.1, -1000),
+    "soc" / DataItemAdapter(Int8ub, "%"),
+    "dcdc_state" / Enum(Int8ub, on=0x01, off=0x02, abnormal=0xfe, invalid=0xff),
     "gear_state" / Int8ub,
-    "insulation_resistance" / Int16ub,
-    "brake_padel" / Int8ub,
-    "accelerator_padel" / Int8ub,
+    # No abnormal/invalid defined here anyway
+    "insulation_resistance" / DataItemAdapter(Int16ub, "kΩ", validation=False),
+    "brake_padel" / DataItemAdapter(Int8ub, "%"),
+    # This one is special, which one may be a boolean(off=0/on=101) or int(0~100%), but I see no necessarity to handle the boolean.
+    "accelerator_padel" / DataItemAdapter(Int8ub, "%"),
 )
 
 """
 GB/T 32960.3-2025 chp7.2.4.1 table10
 """
 whole_vehicle_data_2025 = Struct(
-    "vehicle_state" / Int8ub,
-    "charge_state" / Int8ub,
-    "operate_state" / Int8ub,
-    "vehicle_speed" / Int16ub,
-    "mileage_total" / Int32ub,
-    "voltage_total" / Int16ub,
-    "current_total" / Int16ub,
-    "soc" / Int8ub,
-    "dcdc_state" / Int8ub,
+    "vehicle_state" / Enum(Int8ub, on=0x01, off=0x03, other=0x03, abnormal=0xfe, invalid=0xff),
+    "charge_state" / Enum(Int8ub, parking_charging=0x01, driving_charging=0x02, uncharged=0x03, completed_charging=0x04, abnormal=0xfe, invalid=0xff),
+    "operate_state" / Enum(Int8ub, pure_electric=0x01, hybrid=0x02, fuel=0x03, abnormal=0xfe, invalid=0xff),
+    "vehicle_speed" / DataItemAdapter(Int16ub, "km/h", 0.1),
+    "mileage_total" / DataItemAdapter(Int32ub, "km", 0.1),
+    "voltage_total" / DataItemAdapter(Int16ub, "V", 0.1),
+    "current_total" / DataItemAdapter(Int16ub, "A", 0.1, -3000), # different with 2016 protocol
+    "soc" / DataItemAdapter(Int8ub, "%"),
+    "dcdc_state" / Enum(Int8ub, on=0x01, off=0x02, abnormal=0xfe, invalid=0xff),
     "gear_state" / Int8ub,
-    "insulation_resistance" / Int16ub,
+    "insulation_resistance" / DataItemAdapter(Int16ub, "kΩ"), # 2025 protocol has defined abnormal/invalid
 )
