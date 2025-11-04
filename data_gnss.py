37c2f51f98750bb8c8fd0f7463395fc62419b703
BeidouChangxi <beidoustar@gmail.com>
2025-11-04 16:35:51 +0800

    Change the naming for use them out of the context

--- data_gnss.py
+++ data_gnss.py
@@ -6,9 +6,9 @@ GB/T 32960.3-2025 chp7.2.4.8 table22
 """
 gnss_states = BitStruct(
     "_reserved" / Padding(5),
-    "e_w" /Enum(BitsInteger(1), e=0, w=1),
-    "n_s" / Enum(BitsInteger(1), n=0, s=1),
-    "valid" / Flag,
+    "gnss_e_w" /Enum(BitsInteger(1), e=0, w=1),
+    "gnss_n_s" / Enum(BitsInteger(1), n=0, s=1),
+    "gnss_valid" / Flag,
 )
 
 """
@@ -16,8 +16,8 @@ GB/T 32960.3-2016 chp7.2.3.5 table14, table15
 """
 gnss_data_2016 = Struct(
     "gnss_state" / gnss_states,
-    "longitude" / DataItemAdapter(Int32ub, "°", 0.000001),
-    "latitude" / DataItemAdapter(Int32ub, "°", 0.000001),
+    "gnss_lng" / DataItemAdapter(Int32ub, "°", 0.000001),
+    "gnss_lat" / DataItemAdapter(Int32ub, "°", 0.000001),
 )
 
 """
@@ -25,7 +25,7 @@ GB/T 32960.3-2025 chp7.2.4.8 table21, table22
 """
 gnss_data_2025 = Struct(
     "gnss_state" / gnss_states,
-    "gcs" / Enum(Int8ub, wgs84=1, gcj02=2, other=3),
-    "longitude" / DataItemAdapter(Int32ub, "°", 0.000001),
-    "latitude" / DataItemAdapter(Int32ub, "°", 0.000001),
+    "gnss_gcs" / Enum(Int8ub, wgs84=1, gcj02=2, other=3),
+    "gnss_lng" / DataItemAdapter(Int32ub, "°", 0.000001),
+    "gnss_lat" / DataItemAdapter(Int32ub, "°", 0.000001),
 )
