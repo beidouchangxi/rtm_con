9d83cb28673d3066bf4463bb427e5f3bfbc3eac0
BeidouChangxi <beidoustar@gmail.com>
2025-11-13 14:44:19 +0800

    Rename the fields to a more clear fashion

--- data_cell_volts.py
+++ data_cell_volts.py
@@ -9,9 +9,9 @@ cell_volts_data_2016 = PrefixedArray(Int8ub, LazyBound(lambda: pack_item_2016))
 GB/T 32960.3-2016 anxB.3.5.3.8 tableB.6
 """
 pack_item_2016 = Struct(
-    "pack_index" / DataItemAdapter(Int8ub, ""),
-    "pack_volt" / DataItemAdapter(Int16ub, "V", 0.1),
-    "pack_curr" / DataItemAdapter(Int16ub, "V", 0.1, -3000),
+    "index" / DataItemAdapter(Int8ub, ""),
+    "volt" / DataItemAdapter(Int16ub, "V", 0.1),
+    "curr" / DataItemAdapter(Int16ub, "V", 0.1, -3000),
     "cell_total" / DataItemAdapter(Int16ub, ""),
     "cell_start_index" / Int16ub,
     "cell_volts" / PrefixedArray(Int8ub, DataItemAdapter(Int16ub, "V", 0.001)),
@@ -26,8 +26,8 @@ cell_volts_data_2025 = PrefixedArray(Int8ub, LazyBound(lambda: pack_item_2025))
 GB/T 32960.3-2025 chp7.2.4.2 table12
 """
 pack_item_2025 = Struct(
-    "pack_index" / DataItemAdapter(Int8ub, ""),
-    "pack_volt" / DataItemAdapter(Int16ub, "V", 0.1),
-    "pack_curr" / DataItemAdapter(Int16ub, "V", 0.1, -3000),
+    "index" / DataItemAdapter(Int8ub, ""),
+    "volt" / DataItemAdapter(Int16ub, "V", 0.1),
+    "curr" / DataItemAdapter(Int16ub, "V", 0.1, -3000),
     "cell_volts" / PrefixedArray(Int16ub, DataItemAdapter(Int16ub, "V", 0.001)),
 )
--- data_emotor.py
+++ data_emotor.py
@@ -9,16 +9,16 @@ emotor_data_2016 = PrefixedArray(Int8ub, LazyBound(lambda: emotor_item_2016))
 GB/T 32960.3-2016 chp7.2.3.2 table11
 """
 emotor_item_2016 = Struct(
-    "em_index" / Int8ub,
-    "em_state" / Enum(Int8ub, power_consuming=0x01, power_generating=0x02, off=0x03, idle=0x04, abnormal=0xfe, invalid=0xff),
-    "em_ctrl_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
+    "index" / Int8ub,
+    "state" / Enum(Int8ub, power_consuming=0x01, power_generating=0x02, off=0x03, idle=0x04, abnormal=0xfe, invalid=0xff),
+    "ctrl_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
     # For some strange reason, the offset for speed and torque are marked without unit in GB/T 32960.3-2016
     # Which means, the offset is added before factor, this is different with any other date item
-    "em_speed" / DataItemAdapter(Int16ub, "rpm", 1, -20000), # Not affected
-    "em_torque" / DataItemAdapter(Int16ub, "N·m", 0.1, -2000), # Raw offset 20000 * factor 0.1
-    "em_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
-    "em_ctrl_volt" / DataItemAdapter(Int16ub, "V", 0.1),
-    "em_ctrl_curr" / DataItemAdapter(Int16ub, "A", 0.1, -1000),
+    "speed" / DataItemAdapter(Int16ub, "rpm", 1, -20000), # Not affected
+    "torque" / DataItemAdapter(Int16ub, "N·m", 0.1, -2000), # Raw offset 20000 * factor 0.1
+    "temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
+    "ctrl_volt" / DataItemAdapter(Int16ub, "V", 0.1),
+    "ctrl_curr" / DataItemAdapter(Int16ub, "A", 0.1, -1000),
 )
 
 """
@@ -30,10 +30,10 @@ emotor_data_2025 = "emotors" / PrefixedArray(Int8ub, LazyBound(lambda: emotor_it
 GB/T 32960.3-2025 chp7.2.4.4 table16
 """
 emotor_item_2025 = Struct(
-    "em_index" / Int8ub,
-    "em_state" / Enum(Int8ub, power_consuming=0x01, power_generating=0x02, off=0x03, idle=0x04, abnormal=0xfe, invalid=0xff),
-    "em_ctrl_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
-    "em_speed" / DataItemAdapter(Int16ub, "rpm", 1, -32000),
-    "em_torque" / DataItemAdapter(Int32ub, "N·m", 0.1, -20000),
-    "em_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
+    "index" / Int8ub,
+    "state" / Enum(Int8ub, power_consuming=0x01, power_generating=0x02, off=0x03, idle=0x04, abnormal=0xfe, invalid=0xff),
+    "ctrl_temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
+    "speed" / DataItemAdapter(Int16ub, "rpm", 1, -32000),
+    "torque" / DataItemAdapter(Int32ub, "N·m", 0.1, -20000),
+    "temp" / DataItemAdapter(Int8ub, "℃", 1, -40),
 )
