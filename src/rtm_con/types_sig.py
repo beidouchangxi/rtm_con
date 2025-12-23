90d23ac493724aca4090748a8a0e7713026beb22
BeidouChangxi <beidoustar@gmail.com>
2025-12-23 23:33:54 +0800

    Avoid hardcoded strings, replace them with other ways

--- src/rtm_con/types_sig.py
+++ src/rtm_con/types_sig.py
@@ -36,6 +36,9 @@ class Signature(Construct):
     A pair of fields "data_start_key" and "data_end_key" with peek type should be provided in the context
     to indicate which part of the data is to be signed or verified.
     """
+    pub_keyword = "public_key"
+    pri_keyword = "private_key"
+
     def __init__(self, data_start_key, data_end_key):
         super().__init__()
         self.data_start_key = data_start_key
@@ -64,11 +67,11 @@ class Signature(Construct):
 
     def _parse(self, stream, context, path):
         sig = self.base_con._parse(stream, context, path)
-        public_key = self._find_key_in_context(context, "public_key")
+        public_key = self._find_key_in_context(context, self.pub_keyword)
         if public_key:
             if cryptography is None:
                 raise MissingCryptographyError(f'If you need signature verification, install with extras "sig" or install cryptography manually')
-            elif sig.algo=="rsa":
+            elif sig.algo==sig_algos.rsa:
                 # Verify signature only if public_key found in context
                 signature_bytes = bytes.fromhex(sig.r_value)
                 data_to_verify = self._find_data_in_context(context)
@@ -81,17 +84,17 @@ class Signature(Construct):
                     )
                 except cryptography.exceptions.InvalidSignature:
                     raise PayloadSignatureVerificationError(f"RSA Signature verification failed at path {path}")
-            elif sig.algo=="sm2":
+            elif sig.algo==sig_algos.sm2:
                 # TBD: SM2, which is not supported by cryptography lib yet
                 raise NotImplementedError("SM2 signature verification is not implemented yet")
-            elif sig.algo=="ecc":
+            elif sig.algo==sig_algos.ecc:
                 raise TypeError("ECC signature is not supported by RTM authority")
             else:
                 raise TypeError("The algorighm specified is not supported by protocol")
         return sig
 
     def _build(self, obj, stream, context, path):
-        private_key = self._find_key_in_context(context, "private_key")
+        private_key = self._find_key_in_context(context, self.pri_keyword)
         if private_key:
             # Auto generate signature only if private key is provided
             if cryptography is None:
@@ -104,7 +107,7 @@ class Signature(Construct):
                     cryptography.hazmat.primitives.hashes.SHA256()
                 )
                 build_res = self.base_con.build({
-                    "algo": "rsa",
+                    "algo": sig_algos.rsa,
                     "r_value": signature.hex(),
                     "s_value": "",
                 })
